18b36b8818380b31c0a7d8418848a9d5fbe2cf3b
Juan Sebastian Cabrera <jusekaa@hotmail.com>
2025-10-06 21:01:33 -0500

    first commit

---
+++ src/main/java/com/arka/BackArkaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.arka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackArkaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackArkaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arka/BackArkaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackArkaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
